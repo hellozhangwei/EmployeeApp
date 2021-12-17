0cf695fb498b72bdd145d436d55ad466efff12f7
Wei Zhang <zhangwei1979@outlook.com>
2021-12-17 14:54:47 +0800

    Don't run tests parallelize, otherwise got SQLite3::BusyException: database is locked

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,7 +6,7 @@ DatabaseCleaner[:sequel].strategy = :transaction
 
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
-  parallelize(workers: :number_of_processors)
+  # parallelize(workers: :number_of_processors)
 
   # Add more helper methods to be used by all tests here...
   setup { DatabaseCleaner.start }
