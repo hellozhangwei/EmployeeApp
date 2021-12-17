0326a02b6890b7c295b378fb7a0fe19b68ed579a
Wei Zhang <zhangwei1979@outlook.com>
2021-12-17 15:03:44 +0800

    Add test case for searching employee

--- test/models/employee_test.rb
+++ test/models/employee_test.rb
@@ -1,35 +1,26 @@
 require "test_helper"
 
 class EmployeeTest < ActiveSupport::TestCase
-  test "create employee" do
-    # DB[:employee].delete
-    # DB = Sequel.sqlite()
-    
-    employee = Employee.new(first_name: "David", last_name: "John")
-    employee.save()
 
-    puts "================employee.id========#{employee.id}======="
+  test "create employee" do
+
+    employee = Employee.new(first_name: "David", last_name: "John")
+
+    assert employee.save
 
     savedEmployee = Employee.find(employee.id).first
-    puts "================savedEmployee========#{savedEmployee.id}======="
     assert employee.first_name=="David"
     assert employee.last_name=="John"
 
-    # puts "================result========#{DB[:Employee].count}======="
   end
 
   test "search employee" do
 
-    # employee = Employee.new(first_name: "Wei2", last_name: "Zhang2")
-    # employee.save()
-    # employee.update_attributes(id:1, first_name: "Wei2 update222")
+    employee = Employee.new(first_name: "David", last_name: "John")
+    employee.save
+    employee_list = EmployeeService.new.search 'david'
 
-    # result = employee.search("zhang")
-    # puts "================result========#{result.count}======="
-    # result.each do |em| 
-       # puts "================em========#{em.first_name}======="
-    # end 
-    # assert employee.save, "Saved the employee with firstName and lastName"
- 
+    assert employee_list.first.first_name== 'David'
+   
   end
 end
