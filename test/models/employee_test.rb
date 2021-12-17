cd31edc6f385c5d20916f8e81e63b2e6823f15eb
Wei Zhang <zhangwei1979@outlook.com>
2021-12-17 13:49:14 +0800

    Add test case for creating employee

--- test/models/employee_test.rb
+++ test/models/employee_test.rb
@@ -1,10 +1,35 @@
 require "test_helper"
 
 class EmployeeTest < ActiveSupport::TestCase
-  test "update employee" do
-    employee = Employee.new(first_name: "Wei2", last_name: "Zhang2")
+  test "create employee" do
+    # DB[:employee].delete
+    # DB = Sequel.sqlite()
+    
+    employee = Employee.new(first_name: "David", last_name: "John")
     employee.save()
-    employee.update_attributes(id:1, first_name: "Wei2 update222")
-    assert employee.save, "Saved the employee with firstName and lastName"
+
+    puts "================employee.id========#{employee.id}======="
+
+    savedEmployee = Employee.find(employee.id).first
+    puts "================savedEmployee========#{savedEmployee.id}======="
+    assert employee.first_name=="David"
+    assert employee.last_name=="John"
+
+    # puts "================result========#{DB[:Employee].count}======="
+  end
+
+  test "search employee" do
+
+    # employee = Employee.new(first_name: "Wei2", last_name: "Zhang2")
+    # employee.save()
+    # employee.update_attributes(id:1, first_name: "Wei2 update222")
+
+    # result = employee.search("zhang")
+    # puts "================result========#{result.count}======="
+    # result.each do |em| 
+       # puts "================em========#{em.first_name}======="
+    # end 
+    # assert employee.save, "Saved the employee with firstName and lastName"
+ 
   end
 end
