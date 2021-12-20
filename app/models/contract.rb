374d3c966f555507ee2bd6676b2dda0f6b0094bf
Wei Zhang <zhangwei1979@outlook.com>
2021-12-20 19:02:19 +0800

    Contract date overlap validation and test cases.

--- app/models/contract.rb
+++ app/models/contract.rb
@@ -1,15 +1,32 @@
 class Contract < Sequel::Model
 
-
   def validate
     super
     errors.add(:legal, 'cannot be empty') if !legal || legal.empty?
     validates_date(start_date, end_date)
+    overlap_date(start_date, end_date, legal)
   end
 
   def validates_date(start_date, end_date)
     if(!start_date.nil? && !end_date.nil?)
       errors.add(end_date, "End date should be greater than start date") if end_date < start_date
+    end
+  end
+
+  def overlap_date(start_date, end_date, legal)
+    
+    if start_date.nil? && end_date.nil?
+      contract = Contract.where(legal: legal).first
+      errors.add(end_date, "#{start_date} ~ #{end_date} is overlap with #{contract.start_date} ~ #{contract.end_date}") if !contract.nil?
+    elsif start_date.nil?
+      contract = Contract.where(Sequel.lit('legal = ? AND (start_date is null OR end_date>=?)', legal, end_date)).first
+      errors.add(end_date, "#{start_date} ~ #{end_date} is overlap with #{contract.start_date} ~ #{contract.end_date}") if !contract.nil?
+    elsif end_date.nil?
+      contract = Contract.where(Sequel.lit('legal = ? AND (end_date is null OR start_date<=?)', legal, start_date)).first
+      errors.add(end_date, "#{start_date} ~ #{end_date} is overlap with #{contract.start_date} ~ #{contract.end_date}") if !contract.nil?
+    else
+      contract = Contract.where(Sequel.lit('legal = ? AND ((end_date is null OR start_date is null) OR ((start_date <= ?) and (end_date >= ? )))', legal, end_date, start_date)).first
+      errors.add(end_date, "#{start_date} ~ #{end_date} is overlap with #{contract.start_date} ~ #{contract.end_date}") if !contract.nil?
     end
 
   end
--- test/models/employee_test.rb
+++ test/models/employee_test.rb
@@ -20,23 +20,82 @@ class EmployeeTest < ActiveSupport::TestCase
     end_date = Date.parse('2020-12-31')
     legal = 'Shinetech'
 
-    contract = Contract.new(start_date: start_date, end_date: end_date, legal: legal )
+    contract = Contract.new(start_date: start_date, end_date: end_date, legal: legal)
     assert contract.save
 
     contract = Contract.new(start_date: start_date, end_date: end_date)
     assert !contract.valid?
 
-    contract = Contract.new(end_date: end_date, legal: legal )
+    contract = Contract.new(end_date: end_date, legal: legal)
+    # assert contract.valid?
+
+    contract = Contract.new(start_date: start_date, legal: legal)
+    # assert contract.valid?
+
+
+    # end date > start date
+    contract = Contract.new(start_date: '2020-01-31', end_date: '2020-01-01', legal: legal)
+    assert !contract.valid?
+
+    # overlap if there there is any reocords in db
+    contract = Contract.new(legal: legal)
+    assert !contract.valid?
+
+    #start date is nil and end date is not nil, overlap if the 
+    contract = Contract.new(end_date: end_date, legal: legal)
+    assert !contract.valid?
+
+
+  end
+
+  test "create contract date overlap with end date nil" do
+    start_date = Date.parse('2020-01-01')
+    end_date = Date.parse('2020-12-31')
+    legal = 'Shinetech'
+
+    contract = Contract.new(start_date: start_date, end_date: end_date, legal: legal)
+    assert contract.save
+
+    contract = Contract.new(start_date: start_date, legal: legal)
+    assert !contract.valid?
+  end
+
+  test "create contract date overlap with start date nil" do
+    start_date = Date.parse('2020-01-01')
+    end_date = Date.parse('2020-12-31')
+    legal = 'Shinetech'
+
+    contract = Contract.new(start_date: start_date, end_date: end_date, legal: legal)
+    assert contract.save
+
+    contract = Contract.new(end_date: end_date, legal: legal)
+    assert !contract.valid?
+  end
+
+  test "create contract date overlap with start date and end_date" do
+    start_date = Date.parse('2020-01-01')
+    end_date = Date.parse('2020-01-31')
+    legal = 'Shinetech'
+
+    contract = Contract.new(start_date: start_date, end_date: end_date, legal: legal)
+    assert contract.save
+
+    contract = Contract.new(start_date: Date.parse('2020-01-02'), end_date: Date.parse('2020-02-02'), legal: legal)
+    assert !contract.valid?
+
+    contract = Contract.new(start_date: Date.parse('2020-02-01'), end_date: Date.parse('2020-02-28'), legal: legal)
     assert contract.valid?
 
-    contract = Contract.new(start_date: start_date, legal: legal )
+    contract = Contract.new(start_date: Date.parse('2019-01-01'), end_date: Date.parse('2019-12-31'), legal: legal)
     assert contract.valid?
 
-
-    contract = Contract.new(start_date: '2020-01-31', end_date: '2020-01-01', legal: legal )
+    contract = Contract.new(legal: legal)
     assert !contract.valid?
 
   end
+
+
+
 
   test "search employee" do
 
