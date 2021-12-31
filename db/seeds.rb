0f64de88f5dbf4c8176ec8c6cbd48f3f47f3610a
Wei Zhang <zhangwei1979@outlook.com>
2021-12-31 15:00:24 +0800

    demo date for contract

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
 10.times do
   employee = Employee.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, address: Faker::Address.full_address)
+
+  Contract.create(employee_id:employee.id, start_date: '2020-01-01', end_date: '2020-01-31', legal: 'Shinetech')
 end
