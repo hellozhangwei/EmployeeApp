a3521cb0285703590c9e8cfdab84315eefd8d1e9
Wei Zhang <zhangwei1979@outlook.com>
2022-01-04 13:05:43 +0800

    Search version by sepecifed date

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -6,8 +6,21 @@ class EmployeesController < ApplicationController
   end
 
   def show
+      # SELECT * FROM `employee_versions`
+        # WHERE ((`employee_versions`.`master_id` = 4)
+        # AND (`created_at` <= '2022-01-04 09:20:14.843269')
+        # AND ((`expired_at` IS NULL) OR (`expired_at` > '2022-01-04 09:20:14.843500'))
+        # AND (`valid_from` <= '2022-01-04 09:20:14.843600')
+        # AND (`valid_to` > '2022-01-04 09:20:14.843668')) 
+      # LIMIT 1
     # @employee = Employee.first(id: params[:id])
+
     @employee = Employee.find(id: params[:id])
+
+    date = params[:date] ? params[:date] : Date.today
+
+    @employee_version = EmployeeVersion.where(Sequel.lit('master_id = ? AND (valid_from is null OR valid_from<=?) AND (valid_to is null OR valid_to >=?)', @employee.id, date, date))
+
   end
 
   def edit
@@ -18,7 +31,7 @@ class EmployeesController < ApplicationController
 
     @employee = Employee.find(id: params[:id])
 
-    if(params[:date].empty?)
+    if params[:date].empty?
       result = @employee.update_attributes employee_params
     else
       result = @employee.update_attributes employee_params.merge(valid_from: params[:date])
