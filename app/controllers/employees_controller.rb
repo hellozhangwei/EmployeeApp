6c3b3e9e1a9126e87c4da827b2a542bf354eec88
Wei Zhang <zhangwei1979@outlook.com>
2022-01-10 19:56:55 +0800

    Use Sequel::Plugins::Bitemporal.at(params[:date]) correclty

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -6,20 +6,14 @@ class EmployeesController < ApplicationController
   end
 
   def show
-      # SELECT * FROM `employee_versions`
-        # WHERE ((`employee_versions`.`master_id` = 4)
-        # AND (`created_at` <= '2022-01-04 09:20:14.843269')
-        # AND ((`expired_at` IS NULL) OR (`expired_at` > '2022-01-04 09:20:14.843500'))
-        # AND (`valid_from` <= '2022-01-04 09:20:14.843600')
-        # AND (`valid_to` > '2022-01-04 09:20:14.843668')) 
-      # LIMIT 1
-    # @employee = Employee.first(id: params[:id])
 
     @employee = Employee.find(id: params[:id])
 
     date = params[:date] ? params[:date] : Date.today
 
-    @employee_version = EmployeeVersion.where(Sequel.lit('master_id = ? AND (valid_from is null OR valid_from<=?) AND (valid_to is null OR valid_to >=?)', @employee.id, date, date))
+    Sequel::Plugins::Bitemporal.at(date) do
+      @employee.current_version(:reload => true)
+    end
 
   end
 
@@ -31,26 +25,16 @@ class EmployeesController < ApplicationController
 
     @employee = Employee.find(id: params[:id])
 
-    if params[:date].empty?
-      result = @employee.update_attributes employee_params
-    else
-      result = @employee.update_attributes employee_params.merge(valid_from: params[:date])
-      @employee.contracts.each do |contract|
-        contract.update_attributes(valid_from: params[:date])
-      end
+    Sequel::Plugins::Bitemporal.at(params[:date]) do
+      @employee.update_attributes(first_name: params[:first_name], address: params[:address])
     end
 
-    if result # if you change nothing, it will retruen false and refresh edit page.
-      @employee = Employee.find(id: params[:id])
-      render :show
-    else
-      render :edit
+    Sequel::Plugins::Bitemporal.at(params[:date]) do
+      @employee.current_version(:reload => true)
     end
+     
+    render :edit
+    
   end
 
-  private
-
-  def employee_params
-    params.require(:employee).permit(:first_name, :last_name, :address)
-  end
 end
